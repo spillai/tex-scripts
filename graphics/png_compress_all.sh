df978c65274f2ef58911c2e7c6b6bbfd476498f7
Sudeep Pillai <spillai@csail.mit.edu>
2016-02-13 16:21:09 -0500

    compress all script

--- graphics/png_compress_all.sh
+++ graphics/png_compress_all.sh
@@ -1,5 +1,5 @@
 # Convert to jpgs
-find . -type f \( -iname "*.png" \) -printf "echo %p; convert %p -quality 90 %p.jpg\n" | bash
+find . -type f \( -iname "*.png" \) -printf "echo %p; convert %p -quality 95 %p.jpg\n" | bash
 
 # Rename png.jpg to jpg
 find . -name "*.png.jpg" -exec rename 's/png\.//' {} ";"
