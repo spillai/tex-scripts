789719ab94d04f6fe649893e6b5cfb8e8d3191e9
Sudeep Pillai <spillai@csail.mit.edu>
2017-04-11 17:16:27 -0400

    modified setup_paper.sh

--- setup_paper.sh
+++ setup_paper.sh
@@ -18,3 +18,12 @@ echo "First commit"
 git commit -am 'First commit'
 git remote add origin git@github.mit.edu:spillai/$TITLE.git
 git push -u origin master
+
+# A script to setup a git repo with the tex_scripts submodule
+echo "Set up tex-scripts submodule"
+git submodule add git@github.mit.edu:spillai/tex-scripts.git scripts
+cp scripts/Makefile Makefile
+
+echo "Modify Makefile with correct title"
+
+
